239fdd2b927db5175c035bc10f8d1850d93e9f8b
Sheby <83452219+Shebyyy@users.noreply.github.com>
2026-09-04 20:44:19 +0530

    Refactor last_seen column addition and update query

--- drizzle/0014_add_last_seen.sql
+++ drizzle/0014_add_last_seen.sql
@@ -1,2 +1,5 @@
-ALTER TABLE users ADD COLUMN last_seen integer DEFAULT (unixepoch());
-UPDATE users SET last_seen = (unixepoch()) WHERE last_seen IS NULL;
+ALTER TABLE users ADD COLUMN last_seen INTEGER;
+
+UPDATE users
+SET last_seen = unixepoch()
+WHERE last_seen IS NULL;
